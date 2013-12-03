28805715cb094b0a9b95dab23ba38bbe0fdd7b76
Alex Barauskas <a.barauskas@y-node.com>
2013-12-03 15:16:06 +0300

    .

--- urls.py
+++ urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns(
 
     url(r'^account/logout/$', 'django.contrib.auth.views.logout',
         { 'next_page' : '/' }, name="logout"),
-    url(r'^account/', include('account.urls')),
+    #url(r'^account/', include('account.urls')),
                        
 #    url(r'^login/$','django.contrib.auth.views.login',{ 'template_name' : 'login.html'},name="login"),
 
