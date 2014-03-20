ce8b62e9c0dff4b8e3f0400ff3da5f3adfde4495
Alex Barauskas <a.barauskas@y-node.com>
2014-03-20 13:32:08 +0300

    .

--- map/models.py
+++ map/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from blog.models import Post
+import json
 
 OBJ_CHOICES = (('t', u'Маршрут'),
                ('p', u'Точка'),
