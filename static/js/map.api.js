ecea9d4fe81d2346c5e012341cc3c8ad89471d35
Alex Barauskas <barauskasalex@gmail.com>
2014-05-16 22:48:39 +0300

    Элементы становяться не активными, пока выбираются поординаты.

--- static/js/map.api.js
+++ static/js/map.api.js
@@ -86,6 +86,7 @@ var rovar = {
     },
 
     _showPointInfo : function(point){
+	if(!this._runAddPoint){
 	if(this.currentPoint)
 	    this._hidePointInfo(this.currentPoint);
 	if(this.currentTrack)
@@ -142,7 +143,7 @@ var rovar = {
 			 });
 	    $('#disqus_thread').show();
 	}	
-	
+	}
     },
 
 
@@ -235,6 +236,7 @@ var rovar = {
     },
 
     _showTrackInfo : function(track){
+	if(!this._runAddPoint){
 	var self = this;
 	if(this.currentPoint)
 	    this._hidePointInfo(this.currentPoint);
@@ -292,7 +294,7 @@ var rovar = {
 			 });
 	    $('#disqus_thread').show();
 	}
-
+}
 
     },
 
@@ -469,7 +471,10 @@ var rovar = {
 	$("#add-point-btn").html("+ Добавить точку");
 	$("#add-point-dialog").hide();
 	$("#ajax-errors").html("");
+
 	$("#map").attr('style', "");
+	$('.leaflet-clickable').css('cursor', 'pointer');
+
 	this._runAddPoint = false;
 	if(this._addedPoint){
 	    this.map.removeLayer(this._addedPoint);
@@ -485,6 +490,7 @@ var rovar = {
 	$("#add-point-btn").html("Выберите место на карте (Esc для отмены)");
 	var self = this;
 	$(this.map._container).css('cursor', "url('/static/icons/pin-add.png') "+(this._iconSize*this._kLeft).toString() + ' ' + (this._iconSize-1).toString() +",crosshair");
+	$('.leaflet-clickable').css('cursor', "url('/static/icons/pin-add.png') "+(this._iconSize*this._kLeft).toString() + ' ' + (this._iconSize-1).toString() +",crosshair");
 	__addClick = function(e){self._setCoordinates(e);};
 	this.map.on('mousedown',__addClick);
 	}
@@ -501,7 +507,10 @@ var rovar = {
 	this._addedPoint.addTo(this.map);
 
 	$('input[name="coordinates"]').val('[' + e.latlng.lat.toString() + ', ' + e.latlng.lng.toString() + ']');
+
 	$("#map").attr('style', "");
+	$('.leaflet-clickable').css('cursor', 'pointer');
+
 	this.map.off('mousedown', __addClick);
 	$("#ajax-errors").html("");
 	$("#add-point-dialog .for-clear").val('');
