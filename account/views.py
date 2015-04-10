18c1af9d3542c95dd1056f0f5875d999b00fd48a
denisbalyko <denisbalyko@gmail.com>
2015-04-10 12:08:09 +0300

    added pipeline and python social auth

--- account/views.py
+++ account/views.py
@@ -1,94 +1,73 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
-from django.shortcuts import render_to_response, get_object_or_404
-from django.core.urlresolvers import reverse
-from django.template.context import RequestContext
-from django.core.exceptions import ObjectDoesNotExist
+import json
+from django.core import serializers
+from account.models import User
+from account.forms import UserCreationForm
 from django.conf import settings
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-from django.utils.datastructures import MultiValueDictKeyError
+from django.shortcuts import redirect, render_to_response, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout, login, authenticate
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 
-from account.models import Account, ACCOUNT_BACKENDS
-from account.backends import DenyException
-#from account.backends.twitter import TwitterBackend
-from backends import get_backend
+from onbike.decorators import render_to
+from social.backends.google import GooglePlusAuth
+from social.backends.utils import load_backends
+from social.backends.oauth import BaseOAuth1, BaseOAuth2
+from social.apps.django_app.utils import psa
 
-def _error_page(request):
-    message = u"Ошибка авторизации! Перейдите по ссылке, чтобы повторить попытку."
-    redirect = {'url': reverse('account_login'),
-                'title': u"Попробовать еще раз."}
-    return render_to_response('account/error_page.html',
-                              {'message': message,
-                               'redirect': redirect},
-                              RequestContext(request))    
 
-def login_page(request):
-    #print request.user.is_authenticated()
-    backends = []
-    for b in ACCOUNT_BACKENDS:
-        backends.append({'name': b[0],
-                         'title': b[1],
-                         'img': '/static/account/img/%s.png' % b[0],
-                         'url': reverse('login_start', args=[b[0]])})
-    return render_to_response('account/login.html',
-                              {'backends': backends},
-                              RequestContext(request))
+def context(**extra):
+    return dict({
+        'plus_id': getattr(settings, 'SOCIAL_AUTH_GOOGLE_PLUS_KEY', None),
+        'plus_scope': ' '.join(GooglePlusAuth.DEFAULT_SCOPE),
+        'available_backends': load_backends(settings.AUTHENTICATION_BACKENDS)
+    }, **extra)
 
-def login_start(request, backend_name):
-    backend = get_backend(backend_name)
-    if backend is None:
-        return HttpResponseNotFound()
-    redirect_url = request.META.get('HTTP_REFERER','/')
-    print redirect_url
-    request.session['post_login_redirect'] = redirect_url
-    return_url = "http://%s%s" % (settings.MAIN_HOST,
-                                  reverse('login_return', args=[backend_name]))
-    token, url = backend.get_auth_url(return_to=return_url)
-    request.session['account_token'] = token
-    return HttpResponseRedirect(url)
 
-def login_return(request, backend_name):
-    backend = get_backend(backend_name)
-    redirect_url = request.session.get('post_login_redirect', '/')
-    return_url = "http://%s%s" % (settings.MAIN_HOST,
-                                  reverse('login_return', args=[backend_name]))
-    try:
-        access_token = backend.get_token(request, return_url)
-    except DenyException:
-        return HttpResponseRedirect(redirect_url)
-    except:
-        return _error_page(request)
+@render_to('home_new.html')
+def require_email(request):
+    backend = request.session['partial_pipeline']['backend']
+    return context(email_required=True, backend=backend)
 
-    user_data = backend.get_user_data(access_token)
-    
-    account = Account.objects.filter(id_from_backend=user_data['id'],
-                                     backend=backend_name,
-                                     )
-    if account.count():
-        account = account[0]
-        user = account.user
-        user = authenticate(username=user.username, password=2)
-        login(request, user)
-        return HttpResponseRedirect(redirect_url)
-    if not request.user.is_authenticated():
-        un = "%s - %s" % (user_data['screen_name'], user_data['id'])
-        un = un[:30]
-        user, created = User.objects.get_or_create(username=un)
-        if created:
-            temp_password = User.objects.make_random_password(length=12)
-            user.set_password(temp_password)
-        user.first_name = user_data['name']
-        user.save()
-        user = authenticate(username=user.username, password=2)
+
+def logout(request):
+    """Logs out user"""
+    auth_logout(request)
+    return redirect('/')
+
+
+@psa('social:complete')
+def ajax_auth(request, backend):
+    if isinstance(request.backend, BaseOAuth1):
+        token = {
+            'oauth_token': request.REQUEST.get('access_token'),
+            'oauth_token_secret': request.REQUEST.get('access_token_secret'),
+        }
+    elif isinstance(request.backend, BaseOAuth2):
+        token = request.REQUEST.get('access_token')
     else:
-        user = request.user
-    account = Account(user=user,
-                      name=user.first_name,
-                      img_url=user_data['picture'],
-                      backend=backend_name,
-                      id_from_backend=user_data['id'],
-                      ) 
-    account.save()
+        raise HttpResponseBadRequest('Wrong backend type')
+    user = request.backend.do_auth(token, ajax=True)
     login(request, user)
-    return HttpResponseRedirect(redirect_url)
+    data = {'id': user.id, 'username': user.username}
+    return HttpResponse(json.dumps(data), mimetype='application/json')
+
+
+@login_required(login_url='/')
+def edit(request):
+    if request.method == 'GET':
+        form = UserCreationForm(instance=request.user)
+    else:
+        form = UserCreationForm(request.POST, instance=request.user)
+        if form.is_valid() and request.is_ajax():
+            user = form.save()
+            user = authenticate(username=user.username, password='')
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+            data = json.dumps(['success', ])
+        else:
+            data = json.dumps([[k, v] for k, v in form.errors.items()])
+            return HttpResponseBadRequest(data, content_type='application/json')
+        return HttpResponse(data, content_type='application/json')
+    return render(request, 'account/form/edit.html', {'form': form,})
