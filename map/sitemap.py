77a4448b4bdea76dd71d1ca10e492f049155d472
Alex Barauskas <a.barauskas@y-node.com>
2015-04-29 15:18:01 +0300

    .

--- map/sitemap.py
+++ map/sitemap.py
@@ -14,9 +14,9 @@ class LocationSitemap(Sitemap):
     def items(self):
         list1 = [(self._location.name, )]
         list2 = [[self._location.name, t.slug] for t in Type.objects.all() \
-                 if self._location.point_set.filter(type=t).count() or self._location.track_set.filter(type=t).count()]
-        list3 = [[self._location.name, t.slug, o.uid] for o in self._location.point_set.all()]
-        list4 = [[self._location.name, t.slug, o.uid] for o in self._location.track_set.all()]
+                 if self._location.point_set.filter(type=t, state='0').count() or self._location.track_set.filter(type=t, state='0').count()]
+        list3 = [[self._location.name, t.slug, o.uid] for o in self._location.point_set.filter(state='0')]
+        list4 = [[self._location.name, t.slug, o.uid] for o in self._location.track_set.filter(state='0')]
         return list1 + list2 + list3 + list4
 
     def location(self, item):
