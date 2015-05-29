94835940bf3d4f4d37324684ff2b3e13c7461d69
Alex Barauskas <a.barauskas@y-node.com>
2015-05-29 15:26:26 +0300

    .

---
+++ map/translation.py
@@ -0,0 +1,11 @@
+from modeltranslation.translator import translator, TranslationOptions
+from map.models import Point, Track
+
+class PointTranslationOptions(TranslationOptions):
+    fields = ('name', 'description', 'address' )
+
+class TrackTranslationOptions(TranslationOptions):
+    fields = ('name', 'description' )
+
+translator.register(Point, PointTranslationOptions)
+translator.register(Track, TrackTranslationOptions)
