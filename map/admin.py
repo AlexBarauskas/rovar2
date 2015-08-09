ce9c202085e56356e706c391e32b98a86b456fd6
denisbalyko <denisbalyko@gmail.com>
2015-08-09 16:50:10 +0300

    Point Track add in admin

--- map/admin.py
+++ map/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from map.models import Location
+from map.models import Location, Point, Track
 
-
+admin.site.register(Point)
+admin.site.register(Track)
 admin.site.register(Location, admin.ModelAdmin)
