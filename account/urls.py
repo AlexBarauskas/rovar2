18cd9db6e41b08dc55e3ea377e97977a9dd69d96
denisbalyko <denisbalyko@gmail.com>
2015-10-01 22:12:37 +0300

    add profile/edit

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('account.views',
-    url(r'^edit/$', "edit", name="account_edit"),
+    url(r'^edit/$', "account_edit", name="account_edit"),
+    url(r'^profile/edit/$', "profile_edit", name="profile_edit"),
     url(r'^profile/(?P<username>[^/]+)/$', "profile", name="account_profile"),
 )
