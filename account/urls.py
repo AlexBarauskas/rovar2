574ca89c12e7063122627d32812713c5a8202357
denisbalyko <denisbalyko@gmail.com>
2015-10-06 22:35:35 +0300

    comments_edit urls

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('account.views',
     url(r'^edit/$', "account_edit", name="account_edit"),
     url(r'^profile/edit/$', "profile_edit", name="profile_edit"),
+
+    url(r'^comments/edit$', "comments_edit", name="comments_edit"),
     url(r'^profile/(?P<username>[^/]+)/$', "profile", name="account_profile"),
 )
