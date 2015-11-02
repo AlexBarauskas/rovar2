321ac466d6d6ba7e308c5d544ca4d8b88209d5e5
denisbalyko <denisbalyko@gmail.com>
2015-11-02 17:09:25 +0300

    remove old function tpl

--- map/static/js/map.api.js
+++ map/static/js/map.api.js
@@ -21,13 +21,6 @@ o.Image.MAX_RESIZE_WIDTH = 10000;
 var __addClick;
 var debug = true;
 
-function loadTmpl(tmpl, data, target) {
-    $.get(tmpl, function (template) {
-        var rendered = Mustache.render(template, data);
-        $(target).html(rendered);
-    });
-}
-
 var rovar = {
     elements: {'points': {}, 'tracks': {}},
     sort_ids: {},
@@ -560,6 +553,8 @@ var rovar = {
             $(track._data.pointB._icon).show();
 
             var data = track._data;
+
+            console.log(data);
 
             var preview = $('.preview-content').html('');
 
